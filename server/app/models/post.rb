a2e3061b9d1aa174fbac22198ae439c20ed87fd7
Bryan Wodi <talk2kamp@gmail.com>
2018-03-19 00:43:15 -0500

    modified the post model

--- server/app/models/post.rb
+++ server/app/models/post.rb
@@ -15,10 +15,10 @@ class Post < ApplicationRecord
 	:convert_options => {
     :medium => "-quality 100 -strip"}, 
     default_url: "/images/:style/missing.png"
-  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   	validates_attachment :image, presence: true
-  	validates_presence_of :poster
-	validates_presence_of :description
+  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+	validates_attachment_file_name :image, matches: [/png\Z/, /gif\Z/, /jpe?g\Z/]
+  	validates_presence_of :poster, :description, :file_name
 	validates :user, presence: true
 	validates :user_id, presence: true
 end

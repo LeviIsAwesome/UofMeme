261032154d3803deb479f0134de5103ec6b33e28
Bryan Wodi <talk2kamp@gmail.com>
2018-03-19 01:28:42 -0500

    enable mobile apps to request info from API

--- server/config/application.rb
+++ server/config/application.rb
@@ -11,6 +11,15 @@ module UofMeme
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Module definition for uploading files
+    config.middleware.insert_before 0, Rack::Cors do
+    	allow do
+    		origins '*'
+    		resource '*', :headers => :any, :methods => [:get, :post, :options]
+    	end
+    end
+    config.api_only = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

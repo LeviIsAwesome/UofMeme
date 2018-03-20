6fb8811d7742a6840f07da4d280edbeffafbff2f
Bryan Wodi <talk2kamp@gmail.com>
2018-03-20 02:17:34 -0500

    added users controllers for rspec testing

--- server/spec/controllers/users_controller_spec.rb
+++ server/spec/controllers/users_controller_spec.rb
@@ -2,9 +2,21 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+  let(:valid_attributes){
+  	skip("Hash of valid_attributes")
+  }
+
+  let(:invalid_attributes){
+  	skip("Hash of invalid_attributes")
+  }
+
+  let(:valid_session) { {} }
+
   describe "GET #new" do
+
     it "returns http success" do
-      get :new
+      user = User.new! valid_attributes
+      get :new, params: {}, session: valid_session
       expect(response).to have_http_status(:success)
     end
   end
